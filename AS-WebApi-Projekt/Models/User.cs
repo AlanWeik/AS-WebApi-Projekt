7a7ebdbf1085b9aca9bf987e871eaaaf0948ed88
svante1987 <svantepalsson1987@gmail.com>
2021-05-11 15:29:18 +0200

    Liten ändring i User modellen.

--- AS-WebApi-Projekt/Models/User.cs
+++ AS-WebApi-Projekt/Models/User.cs
@@ -8,7 +8,7 @@ namespace AS_WebApi_Projekt.Models
 {
     public class User : IdentityUser
     {
-        public string firstName { get; set; }
-        public string lastName { get; set; }
+        public string firstName { get; set; } = "";
+        public string lastName { get; set; } = "";
     }
 }
