543658e04576067d0cce7a5c72369e57890f6634
Alan Weik <alaan.m@hotmail.com>
2021-05-11 17:15:33 +0200

    New DTO class in place.

---
+++ AS-WebApi-Projekt/DTO/GeoMessageDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AS_WebApi_Projekt.DTO
+{
+    public class V2MessageDTO
+    {
+        public string title { get; set; }
+        public string body { get; set; }
+        public string author { get; set; }
+    }
+
+    public class V2GetDTO
+    {
+        public V2MessageDTO message { get; set; }
+        public double longitude { get; set; }
+        public double latitute { get; set; }
+    }
+    public class V2MessagePostDTO
+    {
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+
+    public class V2PostDTO
+    {
+        public V2MessagePostDTO message { get; set; }
+        public double longitude { get; set; }
+        public double latityde { get; set; }
+    }
+}
