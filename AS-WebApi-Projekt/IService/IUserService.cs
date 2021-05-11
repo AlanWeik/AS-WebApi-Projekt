12128bb7f990dabaedf28592564f23de43e429eb
svante1987 <svantepalsson1987@gmail.com>
2021-05-11 15:25:39 +0200

    Installerat diverse Nugets. Lägger till kod för att kontrollera username, password.

---
+++ AS-WebApi-Projekt/IService/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AS_WebApi_Projekt.IService
+{
+    public interface IUserService
+    {
+        bool CheckUser(string username, string password);
+    }
+}
---
+++ AS-WebApi-Projekt/Service/UserService.cs
@@ -0,0 +1,16 @@
+﻿using AS_WebApi_Projekt.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AS_WebApi_Projekt.Service
+{
+    public class UserService : IUserService
+    {
+        public bool CheckUser(string username, string password)
+        {
+            return username.Equals("ThumbIKR") && password.Equals("123");
+        }
+    }
+}
