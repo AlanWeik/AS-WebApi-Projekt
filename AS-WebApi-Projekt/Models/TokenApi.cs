340a3366a054a0b29796541322e33438b6d00815
svante1987 <svantepalsson1987@gmail.com>
2021-05-12 12:24:58 +0200

    token api modell

---
+++ AS-WebApi-Projekt/Models/TokenApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AS_WebApi_Projekt.Models
+{
+    public class TokenApi
+    {
+
+        public class ApiToken
+        {
+            public int Id { get; set; }
+            public User User { get; set; }
+            public string Value { get; set; }
+        }
+    }
+}
