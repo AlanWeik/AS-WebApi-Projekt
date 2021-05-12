0b301804a8bce3895601e7b9598fb0cc14be9a3f
svante1987 <svantepalsson1987@gmail.com>
2021-05-12 11:29:52 +0200

    Flyttat Auth filerna till en ApiKey map, på börjat en token manager funktion.

---
+++ AS-WebApi-Projekt/ApiKey/ApiTokenManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AS_WebApi_Projekt.Data;
+
+namespace AS_WebApi_Projekt.ApiKey
+{
+    public class ApiTokenManager
+    {
+
+
+        private readonly AS_WebApi_ProjektContext _context;
+        public ApiTokenManager(AS_WebApi_ProjektContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
