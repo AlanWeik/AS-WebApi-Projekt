41f64483b6ebb62318b15f3827dabb905c2420e4
Alan Weik <alaan.m@hotmail.com>
2021-05-13 10:05:43 +0200

    All action-methods now wowking. Simplified model for all classes. New DB and migration. General cleaning.

--- AS-WebApi-Projekt/DTO/GeoMessageDTO.cs
+++ AS-WebApi-Projekt/DTO/GeoMessageDTO.cs
@@ -7,33 +7,33 @@ namespace AS_WebApi_Projekt.DTO
 {
     public class V1GetDTO
     {
-        public string message { get; set; }
-        public double longitude { get; set; }
-        public double latitude { get; set; }
+        public string Message { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
     }
     public class V2MessageDTO
     {
-        public string title { get; set; }
-        public string body { get; set; }
-        public string author { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Author { get; set; }
     }
 
     public class V2GetDTO
     {
-        public V2MessageDTO message { get; set; }
-        public double longitude { get; set; }
-        public double latitude { get; set; }
+        public V2MessageDTO Message { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
     }
     public class V2MessagePostDTO
     {
-        public string title { get; set; }
-        public string body { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
     }
 
     public class V2PostDTO
     {
-        public V2MessagePostDTO message { get; set; }
-        public double longitude { get; set; }
-        public double latitude { get; set; }
+        public V2MessagePostDTO Message { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
     }
 }
---
+++ AS-WebApi-Projekt/Models/Model.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AS_WebApi_Projekt.Models
+{
+    public class GeoMessageV1
+    {
+        public int ID { get; set; }
+        public string Message { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+    public class GeoMessageV2 : GeoMessageV1
+    {
+        public new Message Message { get; set; }
+    }
+    public class Message
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Author { get; set; }
+    }
+    public class ApiToken
+    {
+        public int ID { get; set; }
+        public User User { get; set; }
+        public string Value { get; set; }
+    }
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
